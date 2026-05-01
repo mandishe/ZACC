2abb5b105c8ce81ee267538ae10c7b9bde3ccbcd
Edward Mandishe <Edwardmandishe@gmail.com>
2026-05-01 15:19:47 +0200

    configured database successfully.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,7 +7,7 @@ import React, {
 } from "react";
 import { Sidebar } from "./components/Sidebar";
 import { Dashboard } from "./components/Dashboard";
-import { ReportForm } from "./components/ReportForm";
+import ReportForm from "./components/ReportForm";
 import { InvestigatorView } from "./components/InvestigatorView";
 import { CaseTracking } from "./components/CaseTracking";
 import { UserManagement } from "./components/UserManagement";
